55d73cea19c86b60ad08db871f2cc53adeddc46e
benbasty <benbasty@yahoo.fr>
2023-03-07 16:05:19 +0800

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
 import './App.css';
 import Header from './Header';
-// import Solutions from './Solutions';
-// import Cases from './Cases';
-// import Blog from './Blog';
-// import About from './About';
-// import Team from './Team';
-// import Contact from './Contact';
 import Footer from './Footer';
 import BlogPage from './BlogPage';
 import { Route, Routes } from 'react-router-dom';
